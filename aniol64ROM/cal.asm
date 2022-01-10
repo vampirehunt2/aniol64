cdde076da905b68adf5a46948339f9ff38f97d4c
vampirehunt2 <70143882+vampirehunt2@users.noreply.github.com>
2022-01-10 20:04:00 +0100

    entered the ATASCII character set in the character generator

--- aniol64ROM/cal.asm
+++ aniol64ROM/cal.asm
@@ -26,7 +26,7 @@ CalError: defb "Syntax error", 0
 start equ PROGRAM_DATA
 stop equ PROGRAM_DATA + 1
 obi equ PROGRAM_DATA + 2   ; open bracket index
-;cti equ PROGRAM_DATA + 3  ; current token index
+cti equ PROGRAM_DATA + 3  ; current token index
 token equ PROGRAM_DATA + 4 ; 20 byte buffer for the currently parsed token
 tokens equ PROGRAM_DATA + 24 ; array of 2-byte tokens
 
@@ -49,7 +49,9 @@ cal_main_loop:
         LD IX, LineBuff
         LD IY, CalExit
         CALL str_cmp
-        JR Z, _cal_main_exit
+        JR Z, _cal_main_exit ; if the string contains the exit command, quit
+        LD IX, LineBuff ; reload, because str_cmp does not preserve IX
+        CALL cal_tokenize
         JP cal_main_loop
 _cal_error:
         CALL lcd_gotoLn4
@@ -61,6 +63,143 @@ _cal_error:
         CALL lcd_wriStr
         JP cal_main_loop
 _cal_main_exit:
+        RET
+ENDP
+
+PROC
+cal_eval:
+        RET
+ENDP
+
+PROC
+cal_eval_simple:
+        LD IX, tokens     ; initialise the data
+        LD B, 0
+        LD A, (start)
+        LD (cti), A       ; point cti to the start index of the expression
+        LD C, A
+        ADD IX, BC        ; point IX to the first byte of the first token
+        LD A, (stop)
+        LD B, A         ; from now on the stop variable is stored in B
+_loop:
+        LD A, (IX)        ; load the first byte of the current token
+        CP ASTERISK
+        CALL Z, cal_mul
+        CP SLASH
+        CALL Z, cal_div
+        LD A, (cti)
+        CP B
+        JP Z, _end
+        INC IX  ; move two bytes to the next token
+        INC IX
+        INC A
+        INC A
+        LD (cti), A
+_end:
+        RET
+ENDP
+
+cal_prep_operands:
+        PUSH IX
+        DEC IX  ; second byte of the previous token
+        LD A, (IX)
+        LD B, A ; first operand stored in B
+        INC IX
+        INC IX
+        INC IX
+        INC IX ; second byte of the following token
+        LD A, (IX)
+        LD C, A  ; second operand stored in C
+        POP IX
+        RET
+
+PROC
+cal_mul:
+        AND A ; clear carry
+        PUSH BC
+        CALL cal_prep_operands
+_loop:
+        PUSH AF
+        LD A, B
+        CP 0
+        JR Z, _zero
+        POP AF
+        LD A, 0
+        ADD A, C
+        DJNZ _loop
+        JR _end
+_zero:
+        POP AF  ; if first operand is 0 then return 0 in A
+        LD A, 0
+_end:
+        POP BC
+        RET
+ENDP
+
+PROC
+cal_div:
+        PUSH BC
+        CALL cal_prep_operands
+        LD A, B
+        LD B, 0
+        AND A ; clear carry
+_loop:
+        CP C
+        JR C, _end
+        SUB C
+        INC B
+        JR _loop
+_end:
+        LD A, B
+        POP BC
+        RET
+ENDP
+
+cal_add:
+        PUSH BC
+        CALL cal_prep_operands
+        AND A ; clear carry
+        LD A, B
+        ADD A, C
+        POP BC
+        RET
+
+cal_sub:
+        PUSH BC
+        CALL cal_prep_operands
+        LD A, B
+        SUB C
+        POP BC
+        RET
+
+
+
+; breaks down a line into tokens.
+; puts the tokens into the 'tokens' table
+; each token consists of two bytes:
+; operators have the ascii code of the operator and a padding zero
+; operands have a NUMBER constand follwed by the number itself
+; IX - address of the string to tokenize
+PROC
+defb "cal_tokenize"
+cal_tokenize:
+        ; init the variables
+        LD A, -1
+        LD (obi), A
+        LD E, 0 ; current token index
+_tokenizeLoop:
+        CALL str_ltrim
+        CALL str_len
+        CP 0
+        JR Z, _endOfLine
+        CALL cal_nextToken
+        JR _tokenizeLoop
+_endOfLine
+        LD IY, tokens          ; point IY to the beginning of the tokens array
+        LD D, 0
+        ADD IY, DE
+        LD A, EOL
+        LD (IY), A             ; store the EOL to the tokens table at current index
         RET
 ENDP
 
@@ -138,25 +277,4 @@ _error:
         RET
 ENDP
 
-PROC
-defb "cal_tokenize"
-cal_tokenize:
-        ; init the variables
-        LD A, -1
-        LD (obi), A
-        LD E, 0 ; current token index
-_tokenizeLoop:
-        CALL str_ltrim
-        CALL str_len
-        CP 0
-        JR Z, _endOfLine
-        CALL cal_nextToken
-        JR _tokenizeLoop
-_endOfLine
-        LD IY, tokens          ; point IY to the beginning of the tokens array
-        LD D, 0
-        ADD IY, DE
-        LD A, EOL
-        LD (IY), A             ; store the EOL to the tokens table at current index
-        RET
-ENDP
+
