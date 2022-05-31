709e499b9de9ce724a7f90dcfcbc978227eb0f3d
vampirehunt2 <70143882+vampirehunt2@users.noreply.github.com>
2022-05-31 17:47:57 +0200

    test of custom NMI handler

--- aniol64ROM/test/test.asm
+++ aniol64ROM/test/test.asm
@@ -1,9 +1,30 @@
-test_main:
-		LD B, 10
-		LD C, 20
-		CALL vga_gotoXY
-		LD A, '#'
+TestCtr equ PROGRAM_DATA
+
+PROC
+testNmiHandler:
+		LD A, (TestCtr)
+		LD B, A
+		LD A, (NmiCount)
+		AND 00100000b
+		CP B
+		JR NZ, _doTest
+		JR _end		
+_doTest:
+		PUSH AF
+		CALL rnd
 		CALL vga_putChar
+		POP AF
+		JR _end
+_end:
+		LD (TestCtr), A		
+		RET
+ENDP
+
+test_main:
+		LD HL, testNmiHandler
+		CALL registerNmiHandler
+		CALL kbd_readKey
+		CALL resetNmiHandler
 		RET
 
 
