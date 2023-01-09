602718fde66edf51c9fa53ec5985db75ce1d1d32
vampirehunt <silmarillion1@o2.pl>
2023-01-09 17:57:51 +0100

    test and doc updates

---
+++ aniol64ROM/test/build.bat
@@ -0,0 +1,5 @@
+cd C:\8bit\GitHub\aniol64\aniol64ROM\test
+C:\8bit\pasmo-0.6.0\pasmo\mingw\pasmo.exe -v --err --bin --alocal test.asm test.bin
+
+rem C:\Users\PiotrAniola\Documents\8bit\zDevStudio-0.6-source\zDevStudio\bin\pasmo53 -v --err --bin --alocal lib\list.asm lib\list.bin
+pause
