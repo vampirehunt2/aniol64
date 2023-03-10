4f5b654b4f0fe1884e50b22909ac54815d0e4eef
vampirehunt <silmarillion1@o2.pl>
2023-03-10 18:07:50 +0100

    use an env variable to indicate home directory in buildjs.bat

--- aniol64ROM/buildsj.bat
+++ aniol64ROM/buildsj.bat
@@ -1,1 +1,4 @@
-c:\8bit\sjasmplus-1.20.2.win\sjasmplus.exe --raw=c:\8bit\git\aniol64ROM\aniol64.bin --sld=c:\8bit\git\aniol64ROM\aniol64.sld  c:\8bit\git\aniol64ROM\aniol64.asm
+echo %ANIOL_HOME%
+c:\8bit\sjasmplus-1.20.2.win\sjasmplus.exe --raw=%ANIOL_HOME%\aniol64.bin --sld=%ANIOL_HOME%\aniol64.sld  %ANIOL_HOME%\aniol64.asm
+
+pause
