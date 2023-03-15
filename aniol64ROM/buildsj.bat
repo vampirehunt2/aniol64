7da29d540a3c7c273188b40f7cc05755c37b3cac
vampirehunt2 <70143882+vampirehunt2@users.noreply.github.com>
2023-03-15 18:19:27 +0100

    Added cleaniing up old binaries prior to assembly

--- aniol64ROM/buildsj.bat
+++ aniol64ROM/buildsj.bat
@@ -1,4 +1,6 @@
 echo %ANIOL_HOME%
-c:\8bit\sjasmplus-1.20.2.win\sjasmplus.exe --raw=%ANIOL_HOME%\aniol64.bin --sld=%ANIOL_HOME%\aniol64.sld  %ANIOL_HOME%\aniol64.asm
+del %ANIOL_HOME%\aniol64.bin
+del %ANIOL_HOME%\aniol64.sld
+c:\8bit\sjasmplus-1.20.2.win\sjasmplus.exe --raw=%ANIOL_HOME%\aniol64.bin --sld=%ANIOL_HOME%\aniol64.sld --fullpath %ANIOL_HOME%\aniol64.asm
 
 pause
