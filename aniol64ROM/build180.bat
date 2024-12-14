4fa65f3d52c80f9700e6752126ddbf91fa576af1
vampirehunt2 <70143882+vampirehunt2@users.noreply.github.com>
2024-12-14 15:20:19 +0100

    simplified the build script

--- aniol64ROM/build180.bat
+++ aniol64ROM/build180.bat
@@ -1,6 +1,3 @@
-echo %ANIOL_HOME%
-del %ANIOL_HOME%\aniol180.bin
-del %ANIOL_HOME%\aniol180.sld
-c:\8bit\sjasmplus-1.20.2.win\sjasmplus.exe --sym=symbols.txt  --raw=%ANIOL_HOME%\aniol180.bin --sld=%ANIOL_HOME%\aniol180.sld --fullpath %ANIOL_HOME%\aniol180.asm
-
-pause
+del aniol180.bin
+del aniol180.sld
+c:\8bit\sjasmplus-1.20.2.win\sjasmplus.exe --sym=symbols.txt  --raw=aniol180.bin --sld=aniol180.sld --fullpath aniol180.asm
