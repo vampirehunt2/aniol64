c31ca9b6bef9e14e234c16c1bab4cba24f0ebf9c
vampirehunt <silmarillion1@o2.pl>
2026-03-29 20:53:42 +0200

    colour VGA test ROM

--- A64ROM/test/build.bat
+++ A64ROM/test/build.bat
@@ -1,4 +1,4 @@
 echo %ANIOL_HOME%
 del %ANIOL_HOME%\test\test.bin
 del %ANIOL_HOME%\test\test.sld
-c:\8bit\sjasmplus-1.20.2.win\sjasmplus.exe --sym=symbols.txt  --raw=%ANIOL_HOME%\test\test.bin --sld=%ANIOL_HOME%\test\test.sld --fullpath %ANIOL_HOME%\test\testclrvga.asm
+c:\8bit\sjasmplus-1.20.2.win\sjasmplus.exe --sym=symbols.txt  --raw=%ANIOL_HOME%\test\test.bin --sld=%ANIOL_HOME%\test\test.sld --fullpath %ANIOL_HOME%\test\test.asm
--- A64ROM/test/test.asm
+++ A64ROM/test/test.asm
@@ -1,8 +1,24 @@
+; Signed 16-bit multiply: HL * BC → HL
+; Carry flag set if overflow, clear otherwise
+ device NOSLOT64K 
 
 test_main:
-    RET
-
-
-
-
-
+    LD DE, 100
+    LD BC, 15
+;
+; Multiply 16-bit values (with 16-bit result)
+; In: Multiply BC with DE
+; Out: HL = result
+;
+Mult16:
+    ld a,b
+    ld b,16
+Mult16_Loop:
+    add hl,hl
+    sla c
+    rla
+    jr nc,Mult16_NoAdd
+    add hl,de
+Mult16_NoAdd:
+    djnz Mult16_Loop
+    ret
