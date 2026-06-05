7a7127cddfee76b969f6ee55519d0b2f02dd94a8
vampirehunt <silmarillion1@o2.pl>
2026-06-05 20:25:00 +0200

    removed colour definitions
    ...that were supposed to go to to pal.asm

--- A64ROM/dev/ps2.asm
+++ A64ROM/dev/ps2.asm
@@ -17,20 +17,6 @@ MOVE_SE equ 'l'
 MOVE_NW equ '7'
 MOVE_SW equ 'j'
 
-; Colour codes (this card is b&w, but colour codes are needed for compatibility):
-BLACK equ 00h
-BLUE equ 01h
-GREEN equ 02h
-CYAN equ 03h
-OLIVE equ 04h
-SAPPHIRE equ 05h
-RED equ 08h
-PURPLE equ 09h
-YELLOW equ 0Ah
-ORANGE equ 0Ch
-PINK equ 0Dh
-WHITE equ 0Fh
-
 ps2_initSeq:
 		defb 0, 00011000b	; channel reset
 		defb 4, 00000111b	; x1 clock, 1 stop bit, odd parity 
